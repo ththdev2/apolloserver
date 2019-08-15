d7ff0e61ae9cf1efb5f31ffaa507b2a0772ab6d8
Kevin <kevin@Kevinui-iMac.local>
2019-08-15 19:22:07 +0900

    Move Workspace

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,8 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const SECRET = "asdfas98df7";
+
 const Query = {
   users: async (parent, args, { models }) => {
     const Users = await models.User.find();
 
     return Users;
+  },
+  getUser: async (parent, args, { models }) => {
+    const decoded = jwt.verify(args.token, SECRET);
+    const User = await models.User.findOne({ _id: decoded._id });
+
+    if (!User) {
+      throw new Error("Cannot find User");
+    }
+
+    return User;
   }
 };
 
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -3,12 +3,13 @@ import { gql } from "apollo-server";
 const typeDefs = gql`
   type Query {
     users: [User]
+    getUser(token: String!): User
   }
 
   type Mutation {
     register(
       email: String!
-      password: String!
+      password: String
       name: String!
       isAdmin: Boolean
       social: String
@@ -22,7 +23,7 @@ const typeDefs = gql`
     _id: ID!
     name: String!
     email: String!
-    password: String!
+    password: String
     isAdmin: Boolean!
     social: String
   }
